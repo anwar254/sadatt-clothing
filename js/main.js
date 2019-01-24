a1f3dee8fc030e0e5c0d9aa8250231f6b796457b
achiengOchieng <achiengvivy254@gmail.com>
2019-01-24 16:57:32 +0300

    hum hover animation

--- js/main.js
+++ js/main.js
@@ -4,15 +4,6 @@ const hum = document.getElementById("hum");
 console.log(hum)
 
 // listening for mouse click event
-hum.addEventListener('click', ()=>{
-	console.log("mouse clicked")
+hum.addEventListener('click', event => {
+	// hum.classList.add('animate');
 })
-// listening for mouse enter event
-hum.onmouseenter = event => {
-	console.log("mouse enter")
-}
-
-// listening for mouse out event
-hum.onmouseleave = () => {
-	console.log("mouse out")
-}
