804b8e8d15f2341f4166ae624f983edc0bd0b3c0
achiengOchieng <achiengvivy254@gmail.com>
2019-01-24 18:34:06 +0300

    search field

--- js/main.js
+++ js/main.js
@@ -1,14 +1,23 @@
 // animating the humberger menu
 const hum = document.getElementById("hum");
+const srcBtn =document.getElementById('search-btn');
 
-// listening for mouse click event
-hum.addEventListener('click', event => {
-	// hum.classList.add('animate');
+
+function pushed(event) {
 	const elem = event.target;
-
 	if (elem.matches('.pushed')) {
 		elem.classList.remove('pushed')
 	}else{
 		elem.classList.add('pushed')
 	}
+}
+// listening for mouse click event
+hum.addEventListener('click', pushed)
+
+// listening for the search button click event
+srcBtn.addEventListener('click', ()=>{
+	const input = document.getElementById('inp');
+	const inp = input.children[0];
+
+	inp.classList.add('pushed');
 })
